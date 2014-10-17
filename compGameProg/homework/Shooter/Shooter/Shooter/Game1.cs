5e93c53625263968d56420ae6c951decc39e943a
mike <mickety.mike@gmail.com>
2014-10-16 21:19:34 -0500

    Adding first version of falling falcon. Does nothing but load a game without crashing. Animation, Player, and Parallaxing Background classes are compiling, but not yet being used in the game.

---
+++ compGameProg/homework/Shooter/Shooter/Shooter/Game1.cs
@@ -0,0 +1,302 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+
+namespace Shooter
+{
+    /// <summary>
+    /// This is the main type for your game
+    /// </summary>
+    public class Game1 : Microsoft.Xna.Framework.Game
+    {
+        GraphicsDeviceManager graphics;
+        SpriteBatch spriteBatch;
+
+        // Represents the player 
+        Player player;
+        // Keyboard states used to determine key presses
+        KeyboardState currentKeyboardState;
+        KeyboardState previousKeyboardState;
+        // A movement speed for the player
+        float playerMoveSpeed;
+
+        // Image used to display the static background
+        Texture2D mainBackground;
+
+        // Parallaxing Layers
+        ParallaxingBackground bgLayer1;
+        ParallaxingBackground bgLayer2;
+
+        // Enemies
+        Texture2D enemyTexture;
+        List<Enemy> enemies;
+        // The rate at which the enemies appear
+        TimeSpan enemySpawnTime;
+        TimeSpan previousSpawnTime;
+        // A random number generator
+        Random random;
+
+        Animation explosionAnim;
+
+        SoundEffect explosionSoundEffect;
+
+
+        public Game1()
+        {
+            graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+        }
+
+        /// <summary>
+        /// Allows the game to perform any initialization it needs to before starting to run.
+        /// This is where it can query for any required services and load any non-graphic
+        /// related content.  Calling base.Initialize will enumerate through any components
+        /// and initialize them as well.
+        /// </summary>
+        protected override void Initialize()
+        {
+            // TODO: Add your initialization logic here
+            // Initialize the player class
+            player = new Player();
+            // Set a constant player move speed
+            playerMoveSpeed = 8.0f;
+
+            bgLayer1 = new ParallaxingBackground();
+            bgLayer2 = new ParallaxingBackground();
+
+            // Initialize the enemies list
+            enemies = new List<Enemy>();
+            // Set the time keepers to zero
+            previousSpawnTime = TimeSpan.Zero;
+            // Used to determine how fast enemy respawns
+            enemySpawnTime = TimeSpan.FromSeconds(1.0f);
+            // Initialize our random number generator
+            random = new Random();
+
+
+            base.Initialize();
+
+            
+
+
+        }
+
+        /// <summary>
+        /// LoadContent will be called once per game and is the place to load
+        /// all of your content.
+        /// </summary>
+        protected override void LoadContent()
+        {
+            // Create a new SpriteBatch, which can be used to draw textures.
+            spriteBatch = new SpriteBatch(GraphicsDevice);
+
+            // TODO: use this.Content to load your game content here
+
+            // Load the player resources
+            Animation playerAnimation = new Animation();
+            Texture2D playerTexture = Content.Load<Texture2D>("shipAnimation");
+            playerAnimation.Initialize(playerTexture, Vector2.Zero, 115, 69, 8, 30, Color.White, 1f, true);
+
+            Vector2 playerPosition = new Vector2(GraphicsDevice.Viewport.TitleSafeArea.X, GraphicsDevice.Viewport.TitleSafeArea.Y
+            + GraphicsDevice.Viewport.TitleSafeArea.Height / 2);
+            player.Initialize(playerAnimation, playerPosition);
+
+            // Load the parallaxing background
+            bgLayer1.Initialize(Content, "bgLayer1", GraphicsDevice.Viewport.Width, -1);
+            bgLayer2.Initialize(Content, "bgLayer2", GraphicsDevice.Viewport.Width, -2);
+
+            mainBackground = Content.Load<Texture2D>("mainbackground");
+
+            enemyTexture = Content.Load<Texture2D>("mineAnimation");
+
+            explosionAnim = new Animation();
+            Texture2D explosionTexture = Content.Load<Texture2D>("explosion");
+            explosionAnim.Initialize(explosionTexture, Vector2.Zero, 134, 134, 12, 30, Color.White, 1f, false);
+            explosionAnim.Active = false;
+
+            explosionSoundEffect = Content.Load<SoundEffect>(@"Audio\explosion");
+
+
+        }
+
+        /// <summary>
+        /// UnloadContent will be called once per game and is the place to unload
+        /// all content.
+        /// </summary>
+        protected override void UnloadContent()
+        {
+            // TODO: Unload any non ContentManager content here
+        }
+
+        private void AddEnemy()
+        {
+            // Create the animation object
+            Animation enemyAnimation = new Animation();
+            // Initialize the animation with the correct animation information
+            enemyAnimation.Initialize(enemyTexture, Vector2.Zero, 47, 61, 8, 30, Color.White, 1f, true);
+            // Randomly generate the position of the enemy
+            Vector2 position = new Vector2(GraphicsDevice.Viewport.Width + enemyTexture.Width / 2, random.Next(100, GraphicsDevice.Viewport.Height - 100));
+            // Create an enemy
+            Enemy enemy = new Enemy();
+            // Initialize the enemy
+            enemy.Initialize(enemyAnimation, position);
+            // Add the enemy to the active enemies list
+            enemies.Add(enemy);
+        }
+
+        private void UpdateCollisions()
+        {
+            Rectangle playerRect = new Rectangle((int)player.Position.X, (int)player.Position.Y, 
+                                        player.Width, player.Height);
+
+            for(int i=0; i<enemies.Count; i++)
+            {
+                Rectangle enemyRect = new Rectangle((int)enemies[i].Position.X, (int)enemies[i].Position.Y,
+                                        enemies[i].Width, enemies[i].Height);
+
+                if(playerRect.Intersects(enemyRect))
+                {
+                    enemies[i].Health = 0;
+
+                    explosionAnim.Position = new Vector2(playerRect.Center.X, playerRect.Center.Y);
+                    explosionAnim.Active = true;
+
+                    explosionSoundEffect.Play();
+                }
+            }
+        }
+
+        private void UpdateEnemies(GameTime gameTime)
+        {
+            // Spawn a new enemy enemy every 1.5 seconds
+            if (gameTime.TotalGameTime - previousSpawnTime > enemySpawnTime)
+            {
+                previousSpawnTime = gameTime.TotalGameTime;
+
+                // Add an Enemy
+                AddEnemy();
+            }
+
+            // Update the Enemies
+            for (int i = enemies.Count - 1; i >= 0; i--)
+            {
+                enemies[i].Update(gameTime);
+                if (enemies[i].Active == false)
+                {
+                    enemies.RemoveAt(i);
+                }
+            }
+        }
+
+
+        private void UpdatePlayer(GameTime gameTime)
+        {
+            player.Update(gameTime);
+
+            // Use the Keyboard / Dpad
+            if (currentKeyboardState.IsKeyDown(Keys.Left))
+            {
+                player.Position.X -= playerMoveSpeed;
+            }
+            if (currentKeyboardState.IsKeyDown(Keys.Right))
+            {
+                player.Position.X += playerMoveSpeed;
+            }
+            if (currentKeyboardState.IsKeyDown(Keys.Up))
+            {
+                player.Position.Y -= playerMoveSpeed;
+            }
+            if (currentKeyboardState.IsKeyDown(Keys.Down))
+            {
+                player.Position.Y += playerMoveSpeed;
+            }
+            // Make sure that the player does not go out of bounds
+            if (player.Position.X < 0)
+                player.Position.X = 0;
+            if (player.Position.Y < 0)
+                player.Position.Y = 0;
+            if (player.Position.X > Window.ClientBounds.Width - player.Width)
+                player.Position.X = Window.ClientBounds.Width - player.Width;
+            if (player.Position.Y > Window.ClientBounds.Height - player.Height)
+                player.Position.Y = Window.ClientBounds.Height - player.Height;
+        }
+
+
+        /// <summary>
+        /// Allows the game to run logic such as updating the world,
+        /// checking for collisions, gathering input, and playing audio.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Update(GameTime gameTime)
+        {
+            // Allows the game to exit
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed)
+                this.Exit();
+
+            // TODO: Add your update logic here
+
+            // Save the previous state of the keyboard and game pad to determine single key presses
+            previousKeyboardState = currentKeyboardState;
+            // Read the current state of the keyboard and gamepad and store it
+            currentKeyboardState = Keyboard.GetState();
+            //Update the player
+            UpdatePlayer(gameTime);
+
+            // Update the parallaxing background
+            bgLayer1.Update();
+            bgLayer2.Update();
+
+            // Update the enemies
+            UpdateEnemies(gameTime);
+
+            UpdateCollisions();
+
+            explosionAnim.Update(gameTime);
+
+            base.Update(gameTime);
+        }
+
+        /// <summary>
+        /// This is called when the game should draw itself.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+
+            // TODO: Add your drawing code here
+            // Start drawing
+            spriteBatch.Begin();
+
+            spriteBatch.Draw(mainBackground, Vector2.Zero, Color.White);
+
+            // Draw the moving background
+            bgLayer1.Draw(spriteBatch);
+            bgLayer2.Draw(spriteBatch);
+            // Draw the Enemies
+            for (int i = 0; i < enemies.Count; i++)
+            {
+                enemies[i].Draw(spriteBatch);
+            }
+
+
+            // Draw the Player
+            player.Draw(spriteBatch);
+            // Stop drawing
+
+            explosionAnim.Draw(spriteBatch);
+
+            spriteBatch.End();
+
+
+            base.Draw(gameTime);
+        }
+    }
+}
